74426f19d42f90c0aae50887195362b3e9850b58
gonzaloMartinezg <93147447+gonzaloMartinezg@users.noreply.github.com>
2021-11-08 20:19:23 +0100

    maps update 2

--- app/src/main/java/com/example/finalproject_mdp/MapsSportCenters.java
+++ app/src/main/java/com/example/finalproject_mdp/MapsSportCenters.java
@@ -1,18 +1,22 @@
 package com.example.finalproject_mdp;
 
+import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,6 +27,7 @@ import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.finalproject_mdp.databinding.ActivityMapsSportCentersBinding;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MapsSportCenters extends FragmentActivity implements OnMapReadyCallback {
 
@@ -33,10 +38,12 @@ public class MapsSportCenters extends FragmentActivity implements OnMapReadyCall
     Button getPosition;
     Marker markerCurrent;
     int LOCATION_REQUEST_CODE = 10001;
+    LocationManager locationManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getPosition = findViewById(R.id.bt_location);
+
         binding = ActivityMapsSportCentersBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
@@ -47,8 +54,12 @@ public class MapsSportCenters extends FragmentActivity implements OnMapReadyCall
 
         //Location
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        //button
+        getPosition = findViewById(R.id.bt_location);
+
         //validate permission
-   /*     if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+
             getPosition.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -64,7 +75,44 @@ public class MapsSportCenters extends FragmentActivity implements OnMapReadyCall
 
         } else {
             askLocationPermission();
-        } */
+        }
+
+    }
+    public void getCurrLoc() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+      //  Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+        Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (locationGPS != null) {
+            double lat = locationGPS.getLatitude();
+            double longi = locationGPS.getLongitude();
+            gotoLocation(lat,longi);
+        }
+     /*   fusedLocationProviderClient.getLastLocation().addOnCompleteListener(task -> {
+            if (task.isSuccessful()){
+                Location location = task.getResult();
+                gotoLocation(location.getLatitude(),location.getLongitude());
+            }
+        }); */
+
+    }
+
+    private void gotoLocation(double latitude, double longitude) {
+        LatLng latLng = new LatLng(latitude,longitude);
+        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng,18);
+        mMap.moveCamera(cameraUpdate);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
     // to asl permission
  /*   private void askLocationPermission() {
@@ -76,6 +124,34 @@ public class MapsSportCenters extends FragmentActivity implements OnMapReadyCall
             }
         }
     } */
+    // to ask permission
+    private void askLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
+            }
+        }
+    }
+    //Location
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (requestCode == LOCATION_REQUEST_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                //Permission granted
+                getPosition.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        getLocationUser();
+                    }
+                });
+            } else {
+                //Permission not granted
+            }
+        }
+    }
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
@@ -88,6 +164,7 @@ public class MapsSportCenters extends FragmentActivity implements OnMapReadyCall
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
         Double latitud = getIntent().getDoubleExtra("lat",0);
         Double longitud = getIntent().getDoubleExtra("long",0);
 
@@ -97,6 +174,35 @@ public class MapsSportCenters extends FragmentActivity implements OnMapReadyCall
         mMap.moveCamera(CameraUpdateFactory.newLatLng(locSportCenter));
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(locSportCenter,15));
     }
+    private void getLocationUser() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        mMap.setMyLocationEnabled(true); //enable location
+        Task<Location> locationTask = fusedLocationProviderClient.getLastLocation();
+        locationTask.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if(removeMarker == false) {
+                    LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                    markerCurrent = mMap.addMarker(new MarkerOptions().position(latLng).title("Current Position"));
+                    //mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng,10)); it is only to move camera
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 15)); //animate camera
+                    removeMarker = true;
+                }
+            }
+        });
+
+    }
+
   /*  private void getLocationUser() {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             // TODO: Consider calling
